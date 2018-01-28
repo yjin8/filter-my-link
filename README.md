1407166848b4faf633141b5f26fa4c9b1e459701
Yuchen Jin <31979100+yjin8@users.noreply.github.com>
2018-01-28 03:03:05 -0500

    streamlining code

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Filter My Link (FML)
-A Google Chrome extension that filters and replaces certain words
+
+A Google Chrome extension that filters certain words and replaces them with random quotes, phrases or text emojis.
+
+## What's Up?
+This is a Google Chrome Extension originally designed with the primary motivation of fighting against cyber-bullying. Firstly, it is designed to create awareness about the thin line between being friendly and harassing women through digital content that degrade the integrity of woman on social platforms. Also,in a world full of stress, we wish that we erase the negativity offered by the web and  wish to make an effort to bring smile on each individual's face by replacing the offensive contents with an equally humorous words.
+
+We have started incorporating Google's NLP with Sentimental Analysis to understand the user's state of mind and symbolically convert identified offensive words to avoid any collisions.
+
+## Installation
+To install this extension, open ``chrome://extensions`` or go to Menu and select Extensions under More Tools. Make sure that Developer's mode is checked and then click on Load unpacked extension. Select the repository directory and ensure that Filter My Link is enabled. 
+
+## Files
+
+## Challenges
+
+
+## Next Steps
+Allowing users to personalize the list of words that they do or don't want to see. Also, can be extended to provide a "kid-safe-browsing" feature.
+Incorporating Google's Natural Language Processing with Sentimental Analysis to improve the efficiency of the extension.
+Modifying few features to be compatible with messaging apps.
--- content.js
+++ content.js
@@ -1,58 +1,4 @@
-$(document).delegate("#submit", "tap", function() {
-    alert($("#flip-1").val());
-});  
-/*
-function readTextFile(file)
-{
-    var rawFile = new XMLHttpRequest();
-    rawFile.open("GET", file, false);
-    rawFile.onreadystatechange = function ()
-    {
-        if(rawFile.readyState === 4)
-        {
-            if(rawFile.status === 200 || rawFile.status == 0)
-            {
-                var allText = rawFile.responseText;
-                alert(allText);
-            }
-        }
-    }
-    rawFile.send(null);
-}
-
-var data = readTextFile("file://C:\Users\yuche\OneDrive\Documents\GitHub\
-    gender-inequality\extension-skeleton\common_bad.txt");
-
-$(document).ready(function() {
-    $.ajax({
-        type: "GET",
-        url: "common_bad.txt",
-        dataType: "text",
-        success: function(data) {processData(data);}
-     });
-});
-
-function processData(allText) {
-    var record_num = 5;  // or however many elements there are in each row
-    var allTextLines = allText.split(/\r\n|\n/);
-    var entries = allTextLines[0].split(',');
-    var lines = [];
-
-    var headings = entries.splice(0,record_num);
-    while (entries.length>0) {
-        var tarr = [];
-        for (var j=0; j<record_num; j++) {
-            tarr.push(headings[j]+":"+entries.shift());
-        }
-        lines.push(tarr);
-    }
-    // alert(lines);
-}
-*/
-
 var elements = document.getElementsByTagName('*');
-
-//var actualBadWords = ["fuck","dyke","fag","cock","pussy"];
 
 var funLib = ["To succeed in life, you need three things: a wishbone, a backbone and a funny \
             bone", "Nothing is impossible. The word itself says I'm possible!", "Banana", "Orange", "Apple",
@@ -61,9 +7,7 @@ var funLib = ["To succeed in life, you need three things: a wishbone, a backbone
             "Gangs of Wasseypur", "Batman", "Goody Two Shoes"
             ];
 
-var funVerb = ["canoodle", "poo-poo", ]
 
-//var req = new XMLHttpRequest();
 for (var i = 0; i < elements.length; i++) {
     var element = elements[i];
 
@@ -84,7 +28,6 @@ for (var i = 0; i < elements.length; i++) {
             text = text.replace(/meninist/gi, funLib[randInt]);
             text = text.replace(/palin/gi, funLib[randInt]);
 
-<<<<<<< HEAD
             text = text.replace(/shit/gi, funLib[randInt]);
             text = text.replace(/fuck/gi, funLib[randInt]);
             text = text.replace(/damn/gi, funLib[randInt]);
@@ -93,41 +36,16 @@ for (var i = 0; i < elements.length; i++) {
             text = text.replace(/asshole/gi, funLib[randInt]);
             text = text.replace(/bastard/gi, funLib[randInt]);
             text = text.replace(/douche/gi, funLib[randInt]);
-=======
-
-            //if (a !== -1) {
-            //    text = text.replace(/badWords[a]/gi, 'moron');
-            //}
-
-            var quotes = ["To succeed in life, you need three things: a wishbone, a backbone and a funny \
-            bone", "Nothing is impossible. The word itself says I'm possible!", "Banana", "Orange", "Apple",
-            "Live Laugh Love", ":( :( :(", "Juicy Couture", "~*~*~Ghéy~*~*~", "✧・ﾟ:* ", " (ﾉ◕ヮ◕)ﾉ*:・ﾟ✧",
-            "Bed Bath & Beyond", "Cheeto", "Nachos","Taco", "Burrito","dumpling", "( ͡° ͜ʖ ͡°)", "ErMaGerd"
-            ]
-
-
-            var randInt = Math.floor(Math.random() * quotes.length)
->>>>>>> 9f88993f8383c3c0502baa5c95e6d5300b0e1dda
-
 
             text = text.replace(/bitch/gi, funLib[randInt]);
             text = text.replace(/dick/gi, funLib[randInt]);
             text = text.replace(/cock/gi, funLib[randInt]);
 
-            
             text = text.replace(/slut/gi, funLib[randInt]);
             text = text.replace(/pussy/gi, funLib[randInt]);
             text = text.replace(/dyke/gi, "****");
             text = text.replace(/fag/gi, "---");
 
-
-            /*
-            var a = actualBadWords.indexOf(origText);
-
-            if (a !== -1) {
-                text = text.replace(actualBadWords[a], '****');
-            }
-            */
 
             if (text !== origText) {
                 element.replaceChild(document.createTextNode(text), node);
